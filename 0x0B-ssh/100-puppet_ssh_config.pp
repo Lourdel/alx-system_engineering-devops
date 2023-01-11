5a0a83b3e11651ba332b8470cb422fa2399e8894
Lourdel <klourdel@gmail.com>
2023-01-11 19:14:05 +0000

    puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Changes SSH client configuration file using puppet
+
+exec {'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
